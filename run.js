3f60b2e4d4150c8cd403f1e58f41a5060ed80a47
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-09-01 10:58:11 +0900

    Update run.js

--- run.js
+++ run.js
@@ -50,6 +50,14 @@ function checkBatchimEnding(word) {
     return (uni - 44032) % 28 != 0;
 }
 
+function isEmpty(str) {
+
+    if (typeof str == "undefined" || str == null || str == "")
+        return true;
+    else
+        return false;
+}
+
 client.on('messageCreate', async message => {
     const content = message.content;
     const contentArr = content.split(" ");
@@ -70,7 +78,7 @@ client.on('messageCreate', async message => {
 
         var TempArr = [];
 
-        $("div.level-info__expedition > span").each(function (i, elem) {
+        $("div.level-info__expedition > span").each(function (i) {
             TempArr[i] = $(this).text();
         });
 
@@ -78,7 +86,7 @@ client.on('messageCreate', async message => {
 
         var TempArr = [];
 
-        $("div.level-info2__expedition > span").each(function (i, elem) {
+        $("div.level-info2__expedition > span").each(function (i) {
             TempArr[i] = $(this).text();
         });
 
@@ -86,7 +94,7 @@ client.on('messageCreate', async message => {
 
         var TempArr = [];
 
-        $("div.game-info__guild > span").each(function (i, elem) {
+        $("div.game-info__guild > span").each(function (i) {
             TempArr[i] = $(this).text();
         });
 
@@ -122,34 +130,81 @@ client.on('messageCreate', async message => {
         var server = $("span.profile-character-info__server").text();
         server = server.substring(1, server.length);
         const level = $("span.profile-character-info__lv").text();
-        var expedition = $("div.level-info__expedition > span").text();
-        expedition = expedition.substring(6, expedition.length);
-        var itemlevel = $("div.level-info2__expedition > span").text();
-        itemlevel = itemlevel.substring(9, itemlevel.length);
-        var title = $("div.game-info__title").text();
-        title = title.substring(2, itemlevel.length);
-        var guild = $("div.game-info__guild").text();
-        guild = guild.substring(2, itemlevel.length);
+
+        var TempArr = [];
+        $("div.level-info__expedition > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+        const expedition = TempArr[1];
+
+        var TempArr = [];
+        $("div.level-info2__expedition > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+        var itemlevel = TempArr[1];
+
+        var TempArr = [];
+        $("div.game-info__guild > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+        var guild = TempArr[1];
+
+        var TempArr = [];
+        $("div.game-info__title > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+        var title = TempArr[1];
+
         const job = $("img.profile-character-info__img").attr("alt");
         const jobimg = $("img.profile-character-info__img").attr("src");
 
         var wisdom = [];
 
-        $("div.game-info__wisdom > span").each(function (i, elem) {
-            wisdom[i] = $(this).text();
+        $("div.game-info__wisdom > span").each(function (i) {
+            if (isEmpty($(this).text())) {
+                wisdom[i] = "-";
+            } else {
+                wisdom[i] = $(this).text();
+            }
         })
 
         var basicability = [];
 
-        $("div.profile-ability-basic > ul > li > span").each(function (i, elem) {
+        $("div.profile-ability-basic > ul > li > span").each(function (i) {
             basicability[i] = $(this).text();
         });
 
         var battleablility = [];
 
-        $("div.profile-ability-battle > ul > li > span").each(function (i, elem) {
+        $("div.profile-ability-battle > ul > li > span").each(function (i) {
             battleablility[i] = $(this).text();
         });
+
+        var engrave = [];
+
+        $("div.profile-ability-engrave > div > div > ul > li > span").each(function (i) {
+            engrave[i] = $(this).text();
+        })
+
+        var engraveinfo = "";
+
+        for (const item of engrave) {
+            const TempArr = item.split(" Lv.");
+            engraveinfo += `\`\`${TempArr[0]}\`\` : ${TempArr[1]} 레벨\n`;
+        }
+
+        if (engraveinfo == "") {
+            engraveinfo = "각인이 없습니다."
+        }
+
+        for (var i = 1; i < 8; i++) {
+            if (isEmpty(battleablility[i])) {
+                battleablility[i] = "-";
+            }
+            if (isEmpty(basicability[i])) {
+                basicability[i] = "-";
+            }
+        }
 
         const userembed = new MessageEmbed()
             .setColor('#0099ff')
@@ -159,6 +214,7 @@ client.on('messageCreate', async message => {
             .setDescription(`\`\`캐릭터명\`\` : ${userName}\n\`\`서버명\`\` : ${server}\n\`\`직업\`\` : ${job}\n\`\`길드\`\` : ${guild}\n\`\`칭호\`\` : ${title}`)
             .setThumbnail(jobimg)
             .addField('영지 정보', `\`\`영지 이름\`\` : ${wisdom[2]}\n\`\`영지 레벨\`\` : ${wisdom[1]}`, false)
+            .addField('각인 정보', `${engraveinfo}`, false)
             .addField('레벨 정보', `\`\`아이템 레벨\`\` : ${itemlevel}\n\`\`원정대 레벨\`\` : ${expedition}\n\`\`전투 레벨\`\` : ${level}`, true)
             .addField('전투 특성', `\`\`치명\`\` : ${battleablility[1]}\n\`\`특화\`\` : ${battleablility[3]}\n\`\`신속\`\` : ${battleablility[7]}`, true)
             .addField('기본 특성', `\`\`공격력\`\` : ${basicability[1]}\n\`\`최대 생명력\`\` : ${basicability[3]}`, true)
