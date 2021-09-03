dac9339136f86c486c2fd3d62a4b29713b17b52d
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-09-04 00:11:08 +0900

    Revert "modulization"
    This reverts commit c5df1067647a2fa646b136ee5cf4aaa0226514b7.

--- run.js
+++ run.js
@@ -1,13 +1,37 @@
 // Ext lib
-const fs = require('fs');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const Youtube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const playdl = require('play-dl');
+const {
+    youtubeAPI
+} = require('./config.json');
+const youtube = new Youtube(youtubeAPI);
 
 // Require the necessary discord.js classes
+const fs = require('fs');
+
+const {
+    VoiceConnection,
+    VoiceConnectionStatus,
+    AudioPlayerStatus,
+    StreamType,
+    createAudioPlayer,
+    createAudioResource,
+    joinVoiceChannel,
+    NoSubscriberBehavior,
+    getVoiceConnection
+} = require('@discordjs/voice');
+
 const {
     Client,
+    VoiceChannel,
     Collection,
     MessageActionRow,
     MessageButton,
     MessageEmbed,
+    Permissions,
     Intents
 } = require('discord.js');
 
@@ -15,12 +39,6 @@ const {
     token
 } = require('./config.json');
 
-// Require Modules
-const KKOMI = require('./kkomi.js');
-
-const Lostark = require('./lostark.js');
-
-const Music = require('./music.js');
 
 // Create a new client instance
 const client = new Client({
@@ -57,25 +75,455 @@ function checkBatchimEnding(word) {
     return (uni - 44032) % 28 != 0;
 }
 
+function isEmpty(str) {
+
+    if (typeof str == "undefined" || str == null || str == "")
+        return true;
+    else
+        return false;
+}
+
+var MusicData = {
+    queue: [],
+    isPlaying: false,
+    QueueLock: false
+};
+
+var LastConnection;
+
+const MusicPlayer = createAudioPlayer();
+
+MusicPlayer.on('stateChange', (oldState, newState) => {
+    console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+    if (newState.status === AudioPlayerStatus.Idle && oldState.status !== AudioPlayerStatus.Idle) {
+        processQueue();
+    }
+});
+
+async function processQueue() {
+    if (MusicData.QueueLock || MusicPlayer.state.status !== AudioPlayerStatus.Idle || MusicData.queue.length === 0) {
+        return;
+    }
+
+    MusicData.QueueLock = true;
+
+    const nextTrack = MusicData.queue.shift();
+
+    try {
+        let source = await playdl.stream(nextTrack.url);
+
+        const resource = createAudioResource(source.stream, {
+            inputType: source.type
+        });
+
+        MusicPlayer.play(resource);
+
+        const nowplaying = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`지금 재생중 - ${nextTrack.title}`)
+            .setDescription(`${nextTrack.desc}`)
+            .setImage(nextTrack.thumbnail)
+            .setTimestamp()
+            .setFooter('꼬미봇 플레이어 - 꼬미봇 by 아뀨');
+
+        if (MusicData.queue[0]) {
+            nowplaying.addField('다음곡', MusicData.queue[0].title);
+        }
+
+        nextTrack.message.channel.send({
+            embeds: [nowplaying]
+        });
+
+        MusicData.QueueLock = false;
+    } catch (error) {
+        console.log(error);
+        MusicData.QueueLock = false;
+        return processQueue();
+    }
+}
+async function playMusic(connection, message) {
+    const voicechannel = message.member.voice.channel;
+
+    if (MusicPlayer.state.status !== AudioPlayerStatus.Playing) {
+        if (MusicData.queue[0]) {
+            const nextTrack = MusicData.queue.shift();
+
+            let source = await playdl.stream(nextTrack.url);
+
+            const resource = createAudioResource(source.stream, {
+                inputType: source.type
+            });
+
+            MusicPlayer.play(resource);
+
+            connection.subscribe(MusicPlayer);
+
+            const nowplaying = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(`지금 재생중 - ${nextTrack.title}`)
+                .setDescription(`${nextTrack.desc}`)
+                .setImage(nextTrack.thumbnail)
+                .setTimestamp()
+                .setFooter('꼬미봇 플레이어 - 꼬미봇 by 아뀨');
+
+            await message.channel.send({
+                embeds: [nowplaying]
+            });
+        }
+    } else if (voicechannel.id != message.guild.me.voice.channel.id) {
+        const LastAdded = MusicData.queue[MusicData.queue.length - 1];
+
+        MusicData.queue = [];
+
+        MusicData.queue.push(LastAdded);
+
+        const nextTrack = MusicData.queue.shift();
+
+        let source = await playdl.stream(nextTrack.url);
+
+        const resource = createAudioResource(source.stream, {
+            inputType: source.type
+        });
+
+        MusicPlayer.play(resource);
+
+        connection.subscribe(MusicPlayer);
+
+        const nowplaying = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`지금 재생중 - ${nextTrack.title}`)
+            .setDescription(`${nextTrack.desc}`)
+            .setImage(nextTrack.thumbnail)
+            .setTimestamp()
+            .setFooter('꼬미봇 플레이어 - 꼬미봇 by 아뀨');
+
+        await message.channel.send({
+            embeds: [nowplaying]
+        });
+    } else {
+        const queueembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`재생 예약됨 - ${MusicData.queue[MusicData.queue.length - 1].title}`)
+            .setDescription(`${MusicData.queue[MusicData.queue.length - 1].desc}`)
+            .setImage(MusicData.queue[MusicData.queue.length - 1].thumbnail)
+            .setTimestamp()
+            .setFooter('꼬미봇 플레이어 - 꼬미봇 by 아뀨');
+
+        await message.channel.send({
+            embeds: [queueembed]
+        });
+    }
+}
+
+
+
 client.on('messageCreate', async message => {
     const content = message.content;
     const contentArr = content.split(" ");
     const command = contentArr[0];
+    const parameter = contentArr[1];
 
     if (command === '!공지') {
-        KKOMI.Alert(message);
+        if (message.member.roles.cache.has('882486032841453678')) {
+            let today = new Date();
+
+            const alertembed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(`${today.getMonth() + 1}월 ${today.getDate()}일 꼬미봇 안내`)
+                .setDescription(`${message.content.substring(3, message.content.length)}`)
+                .setTimestamp()
+                .setFooter('꼬미봇 공지 - 꼬미봇 by 아뀨');
+
+            channel = client.channels.cache.get('882485876075167806');
+            channel.send({
+                embeds: [alertembed]
+            });
+        } else {
+            await message.channel.send("관리자만 수행할 수 있습니다.");
+        }
     } else if (command === '!인증') {
-        Lostark.Auth(message);
+        const encodeNickName = encodeURI(parameter);
+        const html = await axios.get(`https://lostark.game.onstove.com/Profile/Character/${encodeNickName}`);
+        const $ = cheerio.load(html.data);
+
+        const userName = $("span.profile-character-info__name").text();
+
+        var server = $("span.profile-character-info__server").text();
+        server = server.substring(1, server.length);
+
+        const level = $("span.profile-character-info__lv").text();
+
+        var TempArr = [];
+
+        $("div.level-info__expedition > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+
+        const expedition = TempArr[1];
+
+        var TempArr = [];
+
+        $("div.level-info2__expedition > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+
+        var itemlevel = TempArr[1];
+
+        var TempArr = [];
+
+        $("div.game-info__guild > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+
+        var guild = TempArr[1];
+
+        const userembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('인증 완료')
+            .setAuthor('쪼꼬미 길드 인증 시스템')
+            .setDescription(`\`\`캐릭터명\`\` : ${userName}\n\`\`서버명\`\` : ${server}\n\`\`길드\`\` : ${guild}`)
+            .addField('레벨 정보', `\`\`아이템 레벨\`\` : ${itemlevel}\n\`\`원정대 레벨\`\` : ${expedition}\n\`\`전투 레벨\`\` : ${level}`, true)
+            .setTimestamp()
+            .setFooter('정보 조회 - 꼬미봇 by 아뀨');
+
+        if (userName == "") {
+            await message.channel.send("유저를 찾을 수 없습니다.");
+        } else if (guild != "쪼꼬미" || server != "아브렐슈드") {
+            await message.channel.send("쪼꼬미 길드에 가입된 캐릭터만 인증할 수 있습니다.");
+        } else {
+            message.member.roles.add('881208641640890378');
+            message.member.roles.remove('882140536075599872')
+            message.member.setNickname(userName);
+
+            await message.channel.send({
+                embeds: [userembed]
+            });
+        }
     } else if (command === '!유저') {
-        Lostark.User(message);
+        const encodeNickName = encodeURI(parameter);
+        const html = await axios.get(`https://lostark.game.onstove.com/Profile/Character/${encodeNickName}`);
+        const $ = cheerio.load(html.data);
+        const userName = $("span.profile-character-info__name").text();
+        var server = $("span.profile-character-info__server").text();
+        server = server.substring(1, server.length);
+        const level = $("span.profile-character-info__lv").text();
+
+        var TempArr = [];
+        $("div.level-info__expedition > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+        const expedition = TempArr[1];
+
+        var TempArr = [];
+        $("div.level-info2__expedition > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+        var itemlevel = TempArr[1];
+
+        var TempArr = [];
+        $("div.game-info__guild > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+        var guild = TempArr[1];
+
+        var TempArr = [];
+        $("div.game-info__title > span").each(function (i) {
+            TempArr[i] = $(this).text();
+        });
+        var title = TempArr[1];
+
+        const job = $("img.profile-character-info__img").attr("alt");
+        const jobimg = $("img.profile-character-info__img").attr("src");
+
+        var wisdom = [];
+
+        $("div.game-info__wisdom > span").each(function (i) {
+            if (isEmpty($(this).text())) {
+                wisdom[i] = "-";
+            } else {
+                wisdom[i] = $(this).text();
+            }
+        })
+
+        var basicability = [];
+
+        $("div.profile-ability-basic > ul > li > span").each(function (i) {
+            basicability[i] = $(this).text();
+        });
+
+        var battleablility = [];
+
+        $("div.profile-ability-battle > ul > li > span").each(function (i) {
+            battleablility[i] = $(this).text();
+        });
+
+        var engrave = [];
+
+        $("div.profile-ability-engrave > div > div > ul > li > span").each(function (i) {
+            engrave[i] = $(this).text();
+        })
+
+        var engraveinfo = "";
+
+        for (const item of engrave) {
+            const TempArr = item.split(" Lv.");
+            engraveinfo += `\`\`${TempArr[0]}\`\` : ${TempArr[1]} 레벨\n`;
+        }
+
+        if (engraveinfo == "") {
+            engraveinfo = "각인이 없습니다."
+        }
+
+        for (var i = 1; i < 8; i++) {
+            if (isEmpty(battleablility[i])) {
+                battleablility[i] = "-";
+            }
+            if (isEmpty(basicability[i])) {
+                basicability[i] = "-";
+            }
+        }
+
+        const userembed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('기본 정보')
+            .setURL(`https://lostark.game.onstove.com/Profile/Character/${encodeNickName}`)
+            .setAuthor(`${userName} - ${job}`, jobimg)
+            .setDescription(`\`\`캐릭터명\`\` : ${userName}\n\`\`서버명\`\` : ${server}\n\`\`직업\`\` : ${job}\n\`\`길드\`\` : ${guild}\n\`\`칭호\`\` : ${title}`)
+            .setThumbnail(jobimg)
+            .addField('영지 정보', `\`\`영지 이름\`\` : ${wisdom[2]}\n\`\`영지 레벨\`\` : ${wisdom[1]}`, false)
+            .addField('각인 정보', `${engraveinfo}`, false)
+            .addField('레벨 정보', `\`\`아이템 레벨\`\` : ${itemlevel}\n\`\`원정대 레벨\`\` : ${expedition}\n\`\`전투 레벨\`\` : ${level}`, true)
+            .addField('전투 특성', `\`\`치명\`\` : ${battleablility[1]}\n\`\`특화\`\` : ${battleablility[3]}\n\`\`신속\`\` : ${battleablility[7]}`, true)
+            .addField('기본 특성', `\`\`공격력\`\` : ${basicability[1]}\n\`\`최대 생명력\`\` : ${basicability[3]}`, true)
+            .setTimestamp()
+            .setFooter('정보 조회 - 꼬미봇 by 아뀨');
+
+        if (userName == "") {
+            await message.channel.send("유저를 찾을 수 없습니다.");
+        } else {
+            await message.channel.send({
+                embeds: [userembed]
+            });
+        }
     } else if (command === "!재생") {
-        Music.Play(message);
+        const voicechannel = message.member.voice.channel;
+
+        if (!voicechannel) {
+            return message.channel.send("우선 음성 채널에 참여해야 합니다.");
+        }
+
+        try {
+            const searchkeyword = message.content.substring(3, message.content.length);
+
+            var urls = [];
+            var embedcontent = "";
+
+            const videos = await youtube.searchVideos(searchkeyword, 5);
+
+            if (videos.length < 5) return message.channel.send('충분한 노래를 검색하지 못했습니다.');
+
+            for (let i = 0; i < videos.length; i++) {
+                urls[i] = videos[i].url;
+                embedcontent += `**${i + 1} :** ${videos[i].title}\n`;
+            }
+
+            const musicselectembed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(`검색된 음악 리스트`)
+                .setDescription(`${embedcontent}`)
+                .addField('사용법', '원하는 노래의 \`\`숫자\`\`만 입력하세요. 취소는 아무거나 입력하세요.')
+                .setTimestamp()
+                .setFooter('꼬미봇 플레이어 - 꼬미봇 by 아뀨');
+
+            const filter = m => m.author.id === message.author.id;
+
+            message.channel.send({
+                    embeds: [musicselectembed]
+                })
+                .then(sentMessage => {
+                    message.channel.awaitMessages({
+                            filter,
+                            max: 1,
+                            time: 10000,
+                            errors: ['time']
+                        })
+                        .then(async collected => {
+                            answer = collected.first(1);
+                            collected.each(message => message.delete());
+                            sentMessage.delete();
+
+                            selectnum = parseInt(answer);
+
+                            if (selectnum >= 1 && selectnum <= 5) {
+
+                                const index = selectnum - 1;
+                                const url = urls[index];
+                                const title = videos[index].title;
+                                const desc = videos[index].description;
+                                const thumbnail = videos[index].thumbnails.high.url;
+
+                                const songdata = {
+                                    url,
+                                    title,
+                                    desc,
+                                    thumbnail,
+                                    message,
+                                    voicechannel
+                                }
+
+                                MusicData.queue.push(songdata);
+
+                                const connection = joinVoiceChannel({
+                                    channelId: voicechannel.id,
+                                    guildId: message.guild.id,
+                                    adapterCreator: message.guild.voiceAdapterCreator
+                                });
+
+                                LastConnection = connection;
+
+                                playMusic(connection, message);
+                            } else {
+                                message.channel.send(`재생이 취소되었습니다.`);
+                            }
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            message.channel.send("시간이 초과되었습니다.");
+                        });
+                });
+        } catch (err) {
+            console.log(err);
+            return message.channel.send("노래를 불러오는 과정에서 오류가 발생했습니다.");
+        }
     } else if (command === "!나가") {
-        Music.Leave(message);
+        const voicechannel = message.member.voice.channel;
+
+        if (!voicechannel) {
+            return message.channel.send("우선 음성 채널에 참여해야 합니다.");
+        }
+        if (LastConnection) {
+            if (voicechannel.id == message.guild.me.voice.channel.id) {
+                await LastConnection.destroy();
+                await message.channel.send("꼬미봇 플레이어를 종료합니다.");
+            } else {
+                return message.channel.send("봇과 같은 음성 채널에 있어야 합니다.");
+            }
+        }
     } else if (command === "!다음") {
-        Music.Next(message);
+        const voicechannel = message.member.voice.channel;
+
+        if (!voicechannel) {
+            return message.channel.send("우선 음성 채널에 참여해야 합니다.");
+        }
+
+        if (voicechannel.id == message.guild.me.voice.channel.id) {
+            await MusicPlayer.stop();
+        } else {
+            return message.channel.send("봇과 같은 음성 채널에 있어야 합니다.");
+        }
     }
 });
+
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
